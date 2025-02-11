a0178efd9056ea0984bdcf4da9537d37fe7647b2
Alexanderrmagno <22192@portalcedi.edu.mx>
2025-02-11 08:41:47 -0600

    commit 4

--- src/app/formulario/formulario.component.spec.ts
+++ src/app/formulario/formulario.component.spec.ts
@@ -1,24 +1,44 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { FormularioComponent } from './formulario.component';
 
 describe('FormularioComponent', () => {
   let component: FormularioComponent;
   let fixture: ComponentFixture<FormularioComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FormularioComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [FormularioComponent],
+      imports: [FormsModule]
+    })
+      .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(FormularioComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should validate form with empty fields', () => {
+    const errors = component.validateForm();
+    expect(Object.keys(errors).length).toBeGreaterThan(0);
+  });
+
+  it('should add movie when form is valid', () => {
+    component.formData = {
+      movieName: 'Test Movie',
+      releaseYear: '2023',
+      producerName: 'Test Producer',
+      productionHouse: 'Test Studio',
+      mainActor: 'Test Actor'
+    };
+
+    component.onSubmit();
+    expect(component.movies.length).toBe(1);
   });
 });
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,14 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface MovieForm {
+  movieName: string;
+  releaseYear: string;
+  producerName: string;
+  productionHouse: string;
+  mainActor: string;
+}
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.scss'],
+  styleUrls: ['./formulario.component.scss']
 })
-export class FormularioComponent  implements OnInit {
+export class FormularioComponent {
+  formData: MovieForm = {
+    movieName: '',
+    releaseYear: '',
+    producerName: '',
+    productionHouse: '',
+    mainActor: ''
+  };
 
-  constructor() { }
+  movies: MovieForm[] = [];
+  errors: { [key: string]: string } = {};
 
-  ngOnInit() {}
+  validateForm(): { [key: string]: string } {
+    const newErrors: { [key: string]: string } = {};
 
+    if (!this.formData.movieName.trim()) {
+      newErrors['movieName'] = 'El nombre de la película es requerido';
+    }
+    if (!this.formData.releaseYear.trim()) {
+      newErrors['releaseYear'] = 'El año de lanzamiento es requerido';
+    }
+    if (!this.formData.producerName.trim()) {
+      newErrors['producerName'] = 'El nombre del productor es requerido';
+    }
+    if (!this.formData.productionHouse.trim()) {
+      newErrors['productionHouse'] = 'La casa productora es requerida';
+    }
+    if (!this.formData.mainActor.trim()) {
+      newErrors['mainActor'] = 'El protagonista es requerido';
+    }
+
+    return newErrors;
+  }
+
+  onSubmit(): void {
+    const validationErrors = this.validateForm();
+
+    if (Object.keys(validationErrors).length === 0) {
+      this.movies.push({ ...this.formData });
+      this.formData = {
+        movieName: '',
+        releaseYear: '',
+        producerName: '',
+        productionHouse: '',
+        mainActor: ''
+      };
+      this.errors = {};
+    } else {
+      this.errors = validationErrors;
+    }
+  }
 }
